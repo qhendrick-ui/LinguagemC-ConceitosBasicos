aa8d25bf83f82333f6b01614e27c2b0e4a9d0db6
Hendrick <q.hendrick@escola.pr.gov.br>
2025-08-04 09:29:22 -0300

    Criado um arquivo com exnplos de Variáves e Condicionais

---
+++ Variaveis-Condidionas.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    // Criando variáveis de inteiros
+    int x = 20;
+    int y = 18;
+ if (x > y ){
+     // Este é um condicional
+     printf ("20 é maior qua 18");
+ }
+ 
+  
+ //---------------//------------------//
+ printf("\n");
+ //Criando a variável
+ int horario = 20;
+ 
+ //Condicionais
+ if (horario < 18){
+     printf("Bom Dia. Piazada! ");
+ } else{
+     printf("Vai Dormir, Rapaz!");
+ }
+    return 0;
+}
